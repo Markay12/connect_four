96176f011a35feddaaa46c2e1e62eafb9bd8c899
Markay <54555076+Markay12@users.noreply.github.com>
2020-09-30 11:21:42 -0700

    Updated game logic and index out of bounds issue

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ def is_placement_valid(game, column): #check to make sure the placement of the g
     return game[5][column] == 0 #boolean statement to check if placement is valid
 
 def is_player_selection(player_selection): #check player selection 
-    if player_selection >= 1 or player_selection <= 7:
-        return True
-    else:
+    if player_selection < 1 or player_selection > 7:
         return False
+    else:
+        return True
 
 def get_next(game, column):
     for rows in range(6):
@@ -72,91 +72,132 @@ game = game_board() #draws the initial gameboard
 game_over = False #create game_over variable for main game loop
 turn = 0 #initialize turn to 0 which will start with player one
 winner = 0 #winner set to 1 if player one wins and 2 if player 2 wins
+amount_games = 0 #how many games were played
+player_1_win_count = 0 #how many games did player 1 win?
+player_2_win_count = 0 #how many games did player 2 win?
 
-PLAYER1_NAME = input("---------------------------\nPlayer 1.. What is your name? --->  ")
-print("---------------------------\n")
-PLAYER2_NAME = input("---------------------------\nPlayer 2.. What is your name? ---> ")
-print("---------------------------\n")
+
+#Play vs yourself and friend or play against ai?
+print("|||||||||||||||||||||||||||\n---------------------------\nGamemode [1] : Manual Input for both Players\nGamemode [2] : Play game vs AI")
+gamemode = int(input("[1] or [2] ---> "))
+print("---------------------------\n|||||||||||||||||||||||||||\n\n")
+
+PLAYER1_NAME = ""
+PLAYER2_NAME = ""
+
+#ask for two names if play manually
+if gamemode == 1:
+    player1_name = input("---------------------------\nPlayer 1.. What is your name? --->  ")
+    print("---------------------------\n")
+    player2_name = input("---------------------------\nPlayer 2.. What is your name? ---> ")
+    print("---------------------------\n/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\n\n\n")
+
+    #convert names to Title case in case of mistype
+    player1_name = player1_name.title() 
+    player2_name = player2_name.title()
+
+    PLAYER1_NAME = player1_name
+    PLAYER2_NAME = player2_name
+
+elif gamemode == 2:
+    player1_name = input("---------------------------\nWhat is your name? --->  ")
+    print("---------------------------\nYou will be playing against... Mark..")
+
+    player1_name = player1_name.title()
+
+    PLAYER1_NAME = player1_name
+    PLAYER2_NAME = "Mark"
 
 
 while not game_over:
 
-    #init check while going through loop to see if someone has won
-    if player_win(game, 1): #check to see if player one has won
-        winner = 1 #set var to 1 so we know player one won
-        print("---------------------\n/////////////////////\n\nGame Over\n\nPlayer 1 Wins\n\n/////////////////////\n---------------------")
-        play_again = input("Would you like to play again? (Y/n): ")
-        if play_again.lower() == 'y':
-            game = game_board() #reset the game board to zeroes
-            turn = 0 #reset turn to 0
-            print("\n\n\n\n")
-            game_over == False
-        else:
-            game_over = True #endgame
-    elif player_win(game, 2): #check to see if player two has won
-        winner = 2 #set var to 2 so we know player one won
-        print("---------------------\n/////////////////////\n\nGame Over\n\nPlayer 2 Wins\n\n/////////////////////\n---------------------") 
-        play_again = input("Would you like to play again? (Y/n): ")
-        if play_again.lower() == 'y':
-            game = game_board() #reset the game board to zeroes
-            turn = 0 #reset turn to 0
-            print("\n\n\n\n")
-            game_over == False
-        else:
-            game_over = True #endgame
+    #main loop for playing vs person
+    if (gamemode == 1):
 
-    elif turn % 2 == 0: #if turn is an even number it is player one turn
+        #init check while going through loop to see if someone has won
+        if player_win(game, 1): #check to see if player one has won
+            winner = 1 #set var to 1 so we know player one won
+            print("---------------------\n/////////////////////\n\nGame Over\n\nPlayer 1 Wins\n\n////////////////// ///\n---------------------")
+            play_again = input("Would you like to play again? (Y/n): ")
+            if play_again.lower() == 'y':
+                game = game_board() #reset the game board to zeroes
+                turn = 0 #reset turn to 0
+                amount_games = amount_games + 1 #increment the amount of games
+                player_1_win_count = player_1_win_count + 1
+                print("\n\n\n\n")
+                game_over == False
+            else:
+                game_over = True #endgame
+        elif player_win(game, 2): #check to see if player two has won
+            winner = 2 #set var to 2 so we know player one won
+            print("---------------------\n/////////////////////\n\nGame Over\n\nPlayer 2 Wins\n\n////////////////// ///\n---------------------") 
+            play_again = input("Would you like to play again? (Y/n): ")
+            if play_again.lower() == 'y':
+                game = game_board() #reset the game board to zeroes
+                turn = 0 #reset turn to 0
+                amount_games = amount_games + 1 #increment amount of games
+                player_2_win_count = player_2_win_count + 1 #player adds a win
+                print("\n\n\n\n")
+                game_over == False
+            else:
+                game_over = True #endgame
 
-        print(PLAYER1_NAME + " it is your turn\n---------------")
-        player_1_selection = int(input("Choose a column --> ")) #cast input to integer for column selection
-        print("\n") #spacing for game board
+        elif turn % 2 == 0: #if turn is an even number it is player one turn
 
-        player_1_selection = player_1_selection - 1 #columns begin at 1 for user readability
+            print(PLAYER1_NAME + " it is your turn\n---------------")
+            player_1_selection = int(input("Choose a column --> ")) #cast input to integer for column selection
+            print("\n") #spacing for game board
 
-        if is_player_selection(player_1_selection) and is_placement_valid(game, player_1_selection): #if the player input is out of range, game will not play and turn will not increment, gives error message
+            player_1_selection = player_1_selection - 1 #columns begin at 1 for user readability
 
-            
-
-            #placement of the piece
-            chosen_row = get_next(game, player_1_selection) #get the next row that is open for placement
-            place(game, chosen_row, player_1_selection, 1) #choose game, row specified, player column specified, and place a 1 for player 1
-
-            #nump.flip(<matrix>, <axis>) ; in this case we flipped over the x-axis
-            print(nump.flip(game, 0)) # flip the game board to show correct output
-            print("\n") #spacing
-            turn = turn + 1 #player has played, turn gets incremented
+            if is_player_selection(player_1_selection) and is_placement_valid(game, player_1_selection): #if the    player input is out of range, game will not play and turn will not increment, gives error message
 
 
-        else: #if player is within game bounds
-            print("Input out of range\n---Try Again---\n")
+
+                #placement of the piece
+                chosen_row = get_next(game, player_1_selection) #get the next row that is open for placement
+                place(game, chosen_row, player_1_selection, 1) #choose game, row specified, player column   specified, and place a 1 for player 1
+
+                #nump.flip(<matrix>, <axis>) ; in this case we flipped over the x-axis
+                print(nump.flip(game, 0)) # flip the game board to show correct output
+                print("\n") #spacing
+                turn = turn + 1 #player has played, turn gets incremented
 
 
-    elif turn % 2 != 0: #if turn is an odd number it is player two turn
-        print(PLAYER2_NAME + " it is your turn\n------------")
-        player_2_selection = int(input("Choose a column --> "))
-        print("\n") #spacing for game board
+            else: #if player is within game bounds
+                print("Input out of range\n---Try Again---\n")
 
-        player_2_selection = player_2_selection - 1 #columns begin at 1
 
-        if is_player_selection(player_2_selection) and is_placement_valid(game, player_2_selection): #if the player input is out of range, game will not play and turn will not increment, gives error message
-            
-            #placement of the game piece
-            chosen_row = get_next(game, player_2_selection)
-            place(game, chosen_row, player_2_selection, 2) #choose the correct game board, the next open row, player column selection and a 2 for player 2
+        elif turn % 2 != 0: #if turn is an odd number it is player two turn
+            print(PLAYER2_NAME + " it is your turn\n------------")
+            player_2_selection = int(input("Choose a column --> "))
+            print("\n") #spacing for game board
 
-            print(nump.flip(game, 0)) #print the game updated, flip the board as we initially show it starting top down 
-            print("\n")
+            player_2_selection = player_2_selection - 1 #columns begin at 1
 
-            turn = turn + 1 #increment the turns
+            if is_player_selection(player_2_selection) and is_placement_valid(game, player_2_selection): #if the    player input is out of range, game will not play and turn will not increment, gives error message
 
-        else: #if the player makes a correct selection
-            print("Input out of range\n---Try Again---\n")
+                #placement of the game piece
+                chosen_row = get_next(game, player_2_selection)
+                place(game, chosen_row, player_2_selection, 2) #choose the correct game board, the next open row,   player column selection and a 2 for player 2
 
-    
-if winner == 1: #final game win, check variable winner to see which player one and respective message
+                print(nump.flip(game, 0)) #print the game updated, flip the board as we initially show it starting  top down 
+                print("\n")
+
+                turn = turn + 1 #increment the turns
+
+            else: #if the player makes a correct selection
+                print("Input out of range\n---Try Again---\n")
+
+
+if winner == 1 and amount_games == 0: #final game win, check variable winner to see which player one and respective message
     print("---------------------\n/////////////////////\n\nGame Over\n\nPlayer 1 Wins\n\n/////////////////////\n---------------------\n\n")
-if winner == 2:
-   print("---------------------\n/////////////////////\n\nGame Over\n\nPlayer 2 Wins\n\n/////////////////////\n---------------------") 
+elif winner == 2 and amount_games == 0:
+   print("---------------------\n/////////////////////\n\nGame Over\n\nPlayer 2 Wins\n\n/////////////////////\n---------------------")
+elif amount_games > 0 and winner == 1:
+    print("---------------------\n/////////////////////\n\nGame Over\n\nPlayer 1 Wins the Final Game\n\n\n||||||||||||||||||||||||||||||\n FINAL MATCH STATISTICS\n\nAmount of Games Played: " + str(amount_games) +"\n\nPlayer 1 Score --> " + str(player_1_win_count) + "\nPlayer 2 Score --> " + str(player_2_win_count) + "\n\n\n/////////////////////\n---------------------")
+elif amount_games > 0 and winner == 2:
+    print("---------------------\n/////////////////////\n\nGame Over\n\nPlayer 2 Wins the Final Game\n\n\n||||||||||||||||||||||||||||||\n FINAL MATCH STATISTICS\n\nAmount of Games Played: " + str(amount_games) +"\n\nPlayer 1 Score --> " + str(player_1_win_count) + "\nPlayer 2 Score --> " + str(player_2_win_count) + "\n\n\n/////////////////////\n---------------------")
                
 
 
